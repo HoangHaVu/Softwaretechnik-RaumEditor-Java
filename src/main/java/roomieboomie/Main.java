384207595793b7eda6dd20473f02526e2e3792f7
Unknown <marvin.laibold@yahoo.de>
2019-06-21 18:07:52 +0200

    Gradle-Projekt erstellt

---
+++ src/main/java/roomieboomie/Main.java
@@ -0,0 +1,22 @@
+package roomieboomie;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.scene.Scene;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+    Label label = new Label("RooomieBoomie");
+    Scene scene = new Scene(label);
+    primaryStage.setTitle("RoomieBoomie");
+    primaryStage.setScene(scene);
+    primaryStage.show();
+    }
+}
