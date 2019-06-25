1de0a1d7631ceb75a2bc5df7c3dc69ab06e31ed0
mipetera <mike.peters11@web.de>
2019-06-25 10:31:15 +0200

    Nur ein kleiner Test

--- src/main/java/roomieboomie/business/editor/RoomEditor.java
+++ src/main/java/roomieboomie/business/editor/RoomEditor.java
@@ -1,4 +1,25 @@
 package roomieboomie.business.editor;
 
+import roomieboomie.business.item.layout.LayoutItem;
+import roomieboomie.business.item.placable.PlacableItem;
+import roomieboomie.business.room.Room;
+
 public class RoomEditor {
+
+
+    public RoomEditor(){
+
+    }
+
+    public void saveRoom(Room room){
+
+    }
+    public void addItem(LayoutItem item){
+        
+    }
+    
+    public void addItem(PlacableItem item){
+        
+    }
+
 }
