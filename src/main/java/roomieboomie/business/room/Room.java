15f83e206a3866dcf09d0bb7bbbfee7755a36efc
Unknown <marvin.laibold@yahoo.de>
2019-06-25 12:37:40 +0200

    Room, RoomPreview

--- src/main/java/roomieboomie/business/room/Room.java
+++ src/main/java/roomieboomie/business/room/Room.java
@@ -1,4 +1,175 @@
 package roomieboomie.business.room;
 
+import roomieboomie.business.item.layout.LayoutItem;
+import roomieboomie.business.item.placable.PlacableItem;
+
+import java.util.List;
+
+/**
+ * Raum mit allen Informationen inklusive Grundriss-Array in layout
+ */
 public class Room {
+    private RoomPreview roomPreview;
+    private List<PlacableItem> itemList;
+    private List<LayoutItem> walls;
+    private List<LayoutItem> windows;
+    private List<LayoutItem> doors;
+    private byte[][] layout;
+    private int height;
+    private int width;
+    private int startWidth;
+    private int startHeight;
+
+    /**
+     * Erstellt einen neuen Room aus einer RoomPreview heraus. Diese berechnet layout und wird fuer
+     * das Verwalten weiterer Attribute mitgegeben.
+     * @param roomPreview roomPreview des Rooms
+     * @param layout 2D-Array des Grundrisses
+     * @param height Hoehe des Raums im layout
+     * @param width Breite des Raums im layout
+     * @param startX Index, an der der Raum im layout von links aus gesehen beginnt
+     * @param startY Index, an der der Raum im layout von oben aus gesehen beginnt
+     */
+    public Room(RoomPreview roomPreview, byte[][] layout, int height, int width, int startX, int startY) {
+        this.roomPreview = roomPreview;
+        this.layout = layout;
+        this.height = height;
+        this.width = width;
+        this.startWidth = startX;
+        this.startHeight = startY;
+    }
+
+    /**
+     * Liste mit allen PlacableItems, die im Raum platziert werden muessen
+     * @return List mit PlacalbeItem-Objekten
+     */
+    public List<PlacableItem> getItemList() {
+        return itemList;
+    }
+
+    /**
+     * Setzt die Liste mit allen PlacableItems, die im Raum platziert werden muessen
+     * @param itemList List mit PlacalbeItems
+     */
+    public void setItemList(List<PlacableItem> itemList) {
+        this.itemList = itemList;
+    }
+
+    /**
+     * @return Liste mit allen LayoutItems des Typs LayoutItemType.WALL
+     */
+    public List<LayoutItem> getWalls() {
+        return walls;
+    }
+
+    /**
+     * Setzt die Liste der Wand-Objekte
+     * @param walls Liste mit LayoutItems des Typs LayoutItemType.WALL
+     */
+    public void setWalls(List<LayoutItem> walls) {
+        this.walls = walls;
+    }
+
+    /**
+     * @return Liste mit allen LayoutItems des Typs LayoutItemType.WINDOW
+     */
+    public List<LayoutItem> getWindows() {
+        return windows;
+    }
+
+    /**
+     * Setzt die Liste der Window-Objekte
+     * @param windows Liste mit LayoutItems des Typs LayoutItemType.WINDOW
+     */
+    public void setWindows(List<LayoutItem> windows) {
+        this.windows = windows;
+    }
+
+    /**
+     * @return Liste mit allen LayoutItems des Typs LayoutItemType.DOOR
+     */
+    public List<LayoutItem> getDoors() {
+        return doors;
+    }
+
+    /**
+     * Setzt die Liste der Door-Objekte
+     * @param doors Liste mit LayoutItems des Typs LayoutItemType.DOOR
+     */
+    public void setDoors(List<LayoutItem> doors) {
+        this.doors = doors;
+    }
+
+    /**
+     * @return 2D-Byte-Array mit den Grundriss-Informationen
+     */
+    public byte[][] getLayout() {
+        return layout;
+    }
+
+    /**
+     * Setzt das Grundriss-Layout
+     * @param layout 2D-Byte-Array mit den Grundriss-Informationen
+     */
+    public void setLayout(byte[][] layout) {
+        this.layout = layout;
+    }
+
+    /**
+     * @return Hoehe des Raums im layout
+     */
+    public int getHeight() {
+        return height;
+    }
+
+    /**
+     * @return Breite des Raums im layout
+     */
+    public int getWidth() {
+        return width;
+    }
+
+    /**
+     * @return Name des Raums
+     */
+    public String getName(){
+        return this.roomPreview.getName();
+    }
+
+    /**
+     * @return true, wenn der Raum im Level-Modus spielbar ist; false, wenn im Kreativ-Modus
+     */
+    public boolean isLevel(){
+        return roomPreview.isLevel();
+    }
+
+    /**
+     * Aendert den Status des Raums. True, wenn der Raum im Level-Modus spielbar ist; false, wenn im Kreativ-Modus
+     * @param value Wert des Raumtyps
+     */
+    public void setLevel(boolean value){
+        this.roomPreview.setLevel(value);
+    }
+
+    /**
+     * @return Pfad zum Thumbnail-Bild //TODO ?
+     */
+    public String getThumbnail(){
+        return roomPreview.getThumbnail();
+    }
+
+    /**
+     * @return Hoechster erreichter Score
+     */
+    public int getHighestScore() {
+        return roomPreview.getHighestScore();
+    }
+
+    /**
+     * @return Score, der benoetigt wird, um den Raum zu bestehen
+     */
+    public int getNeededScore() {
+        return roomPreview.getNeededScore();
+    }
+
 }
--- src/main/java/roomieboomie/business/room/RoomPreview.java
+++ src/main/java/roomieboomie/business/room/RoomPreview.java
@@ -1,4 +1,138 @@
 package roomieboomie.business.room;
 
+import roomieboomie.business.highscore.HighscoreList;
+import roomieboomie.persistence.JsonHandler;
+
+/**
+ * "Vorschau" eines Rooms. Beinhaltet die wichtigsten Informationen, um einen Raum im Menü darzustellen.
+ * Kann ein Komplettes Raumobjekt erstellen.
+ */
 public class RoomPreview {
+    private String name;
+    private String thumbnail;
+    private int highestScore;
+    private int neededScore;
+    private boolean level;
+    private HighscoreList highscoreList;
+    private JsonHandler jsonHandler;
+    private int height; //Hoehe des Raums, wird createLayout gesetzt
+    private int width; //Breite des Raums, wird createLayout gesetzt
+
+    /**
+     * Erstellt einen neues RoomPreview-Objekt.
+     * @param name Name des Raums
+     * @param thumbnail Pfad zum Vorschaubild TODO ?
+     * @param highestScore Hoechster erreichter Score
+     * @param neededScore Score, der benoetigt wird, um den Raum zu bestehen
+     * @param level true, wenn der Raum im Level-Modus spielbar ist; false, wenn im Kreativ-Modus
+     * @param highscoreList Highscore-Liste fuer den Room
+     */
+    public RoomPreview(String name, String thumbnail, int highestScore, int neededScore, boolean level, HighscoreList highscoreList) {
+        this.name = name;
+        this.thumbnail = thumbnail;
+        this.highestScore = highestScore;
+        this.neededScore = neededScore;
+        this.level = level;
+        this.highscoreList = highscoreList;
+    }
+
+    /**
+     * Liest Daten aus Persistenzschicht und erstellt Informationen fuer ein ganzes Raum-Objekt.
+     * Die Preview selbst wird mitgegeben, damit die hier gespeicherten Attribute weiter zugreifbar sind
+     * @return
+     */
+    public Room getFullRoom(){
+        int startX = 0; //Startpunkt des Raums von links aus TODO
+        int startY = 0; //Startpunkt des Raus von oben aus TODO
+        return new Room(this, createLayout(), getHeight(), getWidth(), startX, startY);
+    }
+
+    /**
+     * @return Name des Raums
+     */
+    public String getName(){
+        return name;
+    }
+
+    /**
+     * @return Pfad zum Thumbnail-Bild //TODO ?
+     */
+    public String getThumbnail(){
+        return thumbnail;
+    }
+
+    /**
+     * @return Hoechster erreichter Score
+     */
+    public int getHighestScore() {
+        return highestScore;
+    }
+
+    /**
+     * @param highestScore Hoechster erreichter Score
+     */
+    public void setHighestScore(int highestScore) {
+        //TODO das muesste aus der HighscoreList berechnet werden, wenn neuer Eintrag
+        this.highestScore = highestScore;
+    }
+
+    /**
+     * @return Score, der benoetigt wird, um den Raum zu bestehen
+     */
+    public int getNeededScore() {
+        return neededScore;
+    }
+
+    /**
+     * @param neededScore Score, der benoetigt wird, um den Raum zu bestehen
+     */
+    public void setNeededScore(int neededScore) {
+        this.neededScore = neededScore;
+    }
+
+    /**
+     * @return true, wenn der Raum im Level-Modus spielbar ist; false, wenn im Kreativ-Modus
+     */
+    public boolean isLevel(){
+        return this.level;
+    }
+
+    /**
+     * Aendert den Status des Raums. True, wenn der Raum im Level-Modus spielbar ist; false, wenn im Kreativ-Modus
+     * @param value Wert des Raumtyps
+     */
+    public void setLevel(boolean value){
+        this.level = value;
+    }
+
+    /**
+     * @return Hoehe des Raums im layout
+     */
+    private int getHeight() {
+        return 0; //TODO aus createLayout berechnen
+    }
+
+    /**
+     * @return Breite des Raums im layout
+     */
+    private int getWidth() {
+        return 0; //TODO aus createLayout berechnen
+    }
+
+    /**
+     * Liest Daten aus Persistenzschicht und erstellt Informationen fuer den Raum-Grundriss.
+     * @return 2D-Byte-Array mit Grundirssinformationen
+     */
+    private byte[][] createLayout() {
+        //TODO ueber jsonHandler laden und umrechnen
+        return null;
+    }
+
+    /**
+     * Fuegt einen neuen Highscore-Eintrag ein
+     */
+    public void addHighscoreRecord(){
+        //TODO highscoreList. ...()
+        setHighestScore(0); //TODO
+    }
 }
